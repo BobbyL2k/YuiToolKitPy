fe0e3b04619c125970246daf4840bd98beaffd59
BobbyL2k <bobby_nh@hotmail.com>
2016-07-29 02:11:26 +0700

    Update get_file_list in utilities.py

--- utilities.py
+++ utilities.py
@@ -1,13 +1,11 @@
-import os.path.isfile
-import os.path.join
-import os.listdir
+import os
 
 from random import randint
 
 
-def get_file_list(path):
+def get_file_list(path, as_path=False):
     "Return List of file in the provided path"
-    file_list = [file_name
+    file_list = [os.path.join(path, file_name) if as_path else file_name
                  for file_name in os.listdir(path)
                  if os.path.isfile(os.path.join(path, file_name))]
     file_list.sort()
